feee1a04fe672c1c55fe842abaffc497568d3620
Bria White <bria.awhite@gmail.com>
2020-08-04 23:38:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,9 +9,9 @@ end
 def assign_rooms(speakers)
   new_arr = []
   speakers.each_with_index do |name, index|
-  return ["Hello, #{name}! You'll be assigned to room #{index+1}!"]
+    new_arr << "Hello, #{name}! You'll be assigned to room #{index+1}!"
  end
- 
+ new_arr
 end  
 
 
