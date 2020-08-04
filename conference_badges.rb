cd4254d939db4f7cff0382e9dee1bc04157e4533
Bria White <bria.awhite@gmail.com>
2020-08-04 23:29:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end  
+
+def batch_badge_creator(array)
+ badge_messages= []
+ array.each do |name|
+   badge_messages << badge_maker(name)
+ end
+ badge_messages
+end  
+
+def assign_rooms(attendees)
+  rooms = []
+  room_num = 1
+  attendees.each_with_index do |name, index|
+  return ["Hello, #{name}! You'll be assigned to room #{index+1}!"]
+ end
+ 
+end  
+
+
